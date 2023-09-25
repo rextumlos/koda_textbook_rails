9a0687eaa7524381c5a48c356ca13ffc7f4bda8d
rextumlos <johnrex412@gmail.com>
2023-09-25 17:07:04 +0800

    Textbook project: Active Record Association progress

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    # Getting all posts, /posts
+    @posts = Post.all
+  end
+
+  def new
+    # Initiating to create a new post, /posts/new
+    @post = Post.new
+  end
+
+  # Creating a new post
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post create failed'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # Getting post by id
+  def show; end
+
+  # Initiate editing a post, /post/:id/edit
+  def edit; end
+
+  # Updating a post
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = 'Post updated successfully'
+      redirect_to posts_path
+    else
+      flash.now[:alert] = 'Post update failed'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # Deleting a post
+  def destroy
+    @post.destroy
+    flash[:notice] = 'Post destroyed successfully'
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root 'welcome#index'
+
+  resources :posts do
+    resources :comments, except: :show
+  end
+end
