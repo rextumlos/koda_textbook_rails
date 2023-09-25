b87d26a4521860fc80c76bd5303cc435e0775afa
rextumlos <johnrex412@gmail.com>
2023-09-25 18:43:39 +0800

    Post: Created soft kill feature when deleting posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,15 @@
 class Post < ApplicationRecord
+  default_scope { where(deleted_at: nil) }
+
   validates :title, presence: true
   validates :content, presence: true
 
   has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+
+  # Overrides the existing destroy method from controller
+  def destroy
+    update(deleted_at: Time.now)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_092616) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_103537) do
   create_table "categories", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_092616) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
   end
 
   add_foreign_key "comments", "posts"
