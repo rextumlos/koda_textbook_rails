a88a08791ce6c5392d28bc2175dff93c2a913c41
rextumlos <johnrex412@gmail.com>
2023-10-02 15:33:03 +0800

    Feedback: Added remark in _forms

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,15 +1,13 @@
 class FeedbacksController < ApplicationController
 
   before_action :set_feedback, only: [:show, :edit, :update, :destroy]
+  before_action :set_remarks, only: [:new, :edit]
 
   def index
     @feedbacks = Feedback.all
     if params[:email].present?
       @feedbacks = @feedbacks.where('email LIKE ?', "%#{params[:email]}%")
     end
-
-    p params[:start_date]
-    p params[:end_date]
 
     if params[:start_date].present? and params[:end_date].present?
       start_date = params[:start_date]
@@ -22,10 +20,6 @@ class FeedbacksController < ApplicationController
       @feedbacks = @feedbacks.order(id: :asc) if params[:sort] == "id"
       @feedbacks = @feedbacks.order(created_at: :desc) if params[:sort] == "created_at"
     end
-
-
-
-
   end
 
   def show; end
@@ -35,10 +29,10 @@ class FeedbacksController < ApplicationController
   end
 
   def create
-    @feedback = Feedback.new(post_params)
+    @feedback = Feedback.new(feedback_params)
     if @feedback.save
       flash[:notice] = 'Post created successfully'
-      redirect_to root_path
+      redirect_to feedbacks_path
     else
       flash.now[:alert] = 'Post create failed'
       render :new, status: :unprocessable_entity
@@ -48,7 +42,7 @@ class FeedbacksController < ApplicationController
   def edit; end
 
   def update
-    if @feedback.update(post_params)
+    if @feedback.update(feedback_params)
       flash[:notice] = 'Feedback updated successfully'
       redirect_to feedbacks_path
     else
@@ -69,7 +63,12 @@ class FeedbacksController < ApplicationController
     @feedback = Feedback.find(params[:id])
   end
 
-  def post_params
-    params.require(:feedback).permit(:email, :message)
+  def set_remarks
+    # Need to map remarks where key is humanize and value is key because "0" and "1" which should be 0, 1 are invalid remarks.
+    @remarks = Feedback.remarks.map {|key, value| [key.humanize, key]}
+  end
+
+  def feedback_params
+    params.require(:feedback).permit(:email, :message, :remark)
   end
 end
