0d79e5753e1d815d9319cd6d68b7d82c25238577
rextumlos <johnrex412@gmail.com>
2023-10-18 15:36:01 +0800

    api/v1: Added serializer for barangay

--- app/controllers/api/v1/barangays_controller.rb
+++ app/controllers/api/v1/barangays_controller.rb
@@ -4,13 +4,13 @@ class Api::V1::BarangaysController < ApplicationController
     barangays = if city
                   city.barangays
                 else
-                  Address::Barangay.all
+                  Address::Barangay.includes(:city).all
                 end
-    render json: barangays
+    render json: barangays, each_serializer: BarangaySerializer
   end
 
   def show
     barangay = Address::Barangay.find(params[:id])
-    render json: barangay
+    render json: barangay, serializer: BarangaySerializer
   end
 end
