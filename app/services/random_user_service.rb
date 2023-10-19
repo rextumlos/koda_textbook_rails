e25a4b9c588f4c747e3325b65c0b9f7dcbc3f816
rextumlos <johnrex412@gmail.com>
2023-10-19 20:16:37 +0800

    API: Created service for RandomUserAPI

---
+++ app/services/random_user_service.rb
@@ -0,0 +1,12 @@
+class RandomUserService 
+  attr_reader :url
+
+  def initialize
+    @url = 'https://randomuser.me/api/'
+  end
+
+  def fetch_random_user
+    @response = RestClient.get @url
+    JSON.parse(@response)["results"]
+  end
+end
