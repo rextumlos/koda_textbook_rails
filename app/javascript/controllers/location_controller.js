dc9d61dee94d4b50238a040de48865e6c61dfa7a
rextumlos <johnrex412@gmail.com>
2023-10-20 17:19:57 +0800

    LocationController: Added and integrated fetch_cities

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="location"
 export default class extends Controller {
-  static targets = ['selectedRegionId', 'selectedProvinceId']
+  static targets = ['selectedRegionId', 'selectedProvinceId', 'selectedCityId']
   fetchProvinces() {
     let provinceTarget = this.selectedProvinceIdTarget
     $(provinceTarget).empty()
@@ -24,7 +24,23 @@ export default class extends Controller {
   }
 
   fetchCities() {
+    let cityTarget = this.selectedCityIdTarget
+    $(cityTarget).empty()
 
+    $.ajax({
+      type: 'GET',
+      url: `/api/v1/provinces/${this.selectedProvinceIdTarget.value}/cities`,
+      dataType: 'json',
+      success: (response) => {
+        console.log(response)
+        $.each(response, function (index, record) {
+          let option = document.createElement('option')
+          option.value = record.id
+          option.text = record.name
+          cityTarget.appendChild(option)
+        })
+      }
+    })
   }
 
   fetchBarangays() {
