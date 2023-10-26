d231ac3f76bb0dafcd71beda822abc464cfc1d61
rextumlos <johnrex412@gmail.com>
2023-10-26 10:20:51 +0800

    Add language switch in EN, CH, TL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource) if is_navigational_format?
   end
@@ -9,5 +10,13 @@ class ApplicationController < ActionController::Base
     else
       posts_path
     end
+  end
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
   end
 end
